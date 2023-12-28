4ccca05741739317f59f39aad4f590f1c868d731
Muratcan Baba <muratcanbaba1@gmail.com>
2023-12-29 01:25:46 +0300

    initial commit

---
+++ pdp/src/SafeComponent.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default class SafeComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null, errorInfo: null };
+  }
+
+  static getDerivedStateFromError(error) {
+    console.log("Error caught:", error);
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error("Component error:", error, errorInfo);
+    this.setState({ error, errorInfo });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h1>Something went wrong.</h1>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
